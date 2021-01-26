ad2a512871863138a81232e0045c5e129f0d53d9
adyaS <sweetadz12@gmail.com>
2021-01-25 23:06:24 -0500

    Added alert for incorrect password

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-export const Register = () => {
+const Register = ({ setAlert }) => {
 	const [formData, setformData] = useState({
 		name: "",
 		email: "",
@@ -17,7 +20,7 @@ export const Register = () => {
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		if (password !== password2) {
-			console.log("Passwords donot match");
+			setAlert("Passwords do not match", "danger", 3000);
 		} else {
 			console.log("success");
 		}
@@ -86,3 +89,9 @@ export const Register = () => {
 		</Fragment>
 	);
 };
+
+Register.propTypes = {
+	setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
